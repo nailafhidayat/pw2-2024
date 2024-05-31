454a8aa1fa692311740df9e0cf40dd9d8d7bcdba
nailafhidayat <nanaila198@gmail.com>
2024-05-31 10:04:10 +0700

    PRAKTIKUM 11

---
+++ PRAKTIKUM 9/app/Http/Controllers/PasienController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pasien;
+use Illuminate\Http\Request;
+
+class PasienController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $pasien = Pasien::get();
+        return view('pasien.index', compact('pasien'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pasien.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // cek validasi
+        $request -> validate([
+            'kode' => 'required|string',
+            'nama' => 'required|string',
+            'tmp_lahir' => 'required|string',
+            'tgl_lahir' => 'required|date',
+            'gender' => 'required|string',
+            'email' => 'required|string',
+            'alamat' => 'required|string',
+        ]);
+         // Create a new Pasien instance with the validated data
+         Pasien::create([
+            'kode' => $request->kode,
+            'nama' => $request->nama,
+            'tmp_lahir' => $request->tmp_lahir,
+            'tgl_lahir' => $request->tgl_lahir,
+            'gender' => $request->gender,
+            'email' => $request->email,
+            'alamat' => $request->alamat,
+        ]);
+         // Redirect to the index page with a success message
+         return redirect()->route('pasiens.index')->with('success', 'Pasien created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pasien $pasien)
+    {
+        //Pass the Pasien instance to the view
+        return view('pasien.show', compact('pasien'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pasien $pasien)
+    {
+        // Pass the Pasien instance to the view
+        return view('pasien.edit', compact('pasien'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pasien $pasien)
+    {
+
+        // Update the Pasien instance
+        $pasien->update($request->all());
+
+        // Redirect to the index page with a success message
+        return redirect()->route('pasiens.index')->with('success', 'Pasien updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pasien $pasien)
+    {
+        // Delete the Pasien instance
+        $pasien->delete();
+
+        // Redirect back to the index page with a success message
+        return redirect()->route('pasiens.index')->with('success', 'Pasien deleted successfully');
+    }
+}
--- PRAKTIKUM 9/routes/web.php
+++ PRAKTIKUM 9/routes/web.php
@@ -1,11 +1,39 @@
 <?php
 
-namespace App\Http\AdminController;
-
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/admin", [AdminController::class, 'index']);
+Route::get('/kabar', function () {
+    return view('kondisi');
+});
+
+Route::get('/nilai', function () {
+    return view('nilai');
+});
+
+Route::get('/admin', [AdminController::class, 'index']);
+// pasien
+Route::get('/admin/pasien', [PasienController::class, 'index'])->name('pasiens.index');
+Route::get('/admin/pasien/create', [PasienController::class, 'create'])->name('pasiens.create');
+Route::post('/admin/pasien/store', [PasienController::class, 'store'])->name('pasiens.store');
+Route::get('/admin/pasien/{pasien}', [PasienController::class, 'show'])->name('pasiens.show');
+Route::get('/admin/pasien/{pasien}/edit', [PasienController::class, 'edit'])->name('pasiens.edit');
+Route::put('/admin/pasien/{pasien}', [PasienController::class, 'update'])->name('pasiens.update');
+Route::delete('/admin/pasien/{pasien}', [PasienController::class, 'destroy'])->name('pasiens.destroy');
+
